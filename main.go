42957dea4649c445727e022b6b5b50df7d995083
abersheeran <me@abersheeran.com>
2024-01-30 14:15:07 +0800

    Add support for custom headers, request body, and file input

--- main.go
+++ main.go
@@ -3,16 +3,35 @@ package main
 import (
 	"flag"
 	"fmt"
+	"io"
 	"os"
+	"strings"
 
 	"github.com/abersheeran/httpbenchmark/core"
 	. "github.com/abersheeran/rgo-error"
 	"github.com/go-resty/resty/v2"
 )
 
+type arrayFlags []string
+
+func (i *arrayFlags) String() string {
+	return strings.Join(*i, ",")
+}
+
+func (i *arrayFlags) Set(value string) error {
+	*i = append(*i, value)
+	return nil
+}
+
 func main() {
 	concurrency := flag.Int("c", 100, "Concurrency")
 	requests := flag.Int("r", 10000, "Requests")
+	method := flag.String("X", "GET", "Method")
+	var headers arrayFlags
+	flag.Var(&headers, "H", "Header")
+	body := flag.String("d", "", "Request body")
+	file := flag.String("f", "", "Request body file path")
+
 	flag.Usage = func() {
 		fmt.Fprintf(os.Stderr, "Usage: %s [options] url\n", os.Args[0])
 		flag.PrintDefaults()
@@ -27,12 +46,48 @@ func main() {
 		panic("Please use: `[options] url`, not `url [options]`")
 	}
 	url := args[0]
+	if *body != "" && *file != "" {
+		panic("Please specify only one of `-b` and `-f`")
+	}
+
+	fmt.Println("Concurrency:", *concurrency)
+	fmt.Println("Requests:", *requests)
+	fmt.Println("Method:", *method)
+	fmt.Println("Headers:", headers)
+	fmt.Println("Body:", *body)
+	fmt.Println("File:", *file)
+	fmt.Println("URL:", url)
+	fmt.Println()
 
 	res := core.Benchmark{
 		Concurrency: *concurrency,
 		Requests:    *requests,
 		CallRequest: func(request *resty.Request) Result[*resty.Response] {
-			return AsResult(request.Get(url))
+			for _, header := range headers {
+				parts := strings.SplitN(header, ":", 2)
+				if len(parts) != 2 {
+					panic(fmt.Errorf("invalid header: %s", header))
+				}
+				request.SetHeader(strings.TrimSpace(parts[0]), strings.TrimSpace(parts[1]))
+			}
+			if *file != "" {
+				file, err := os.Open(*file)
+				if err != nil {
+					panic(err)
+				}
+				defer file.Close()
+
+				bytes, err := io.ReadAll(file)
+				if err != nil {
+					panic(err)
+				}
+				request.SetBody(bytes)
+			}
+			if *body != "" {
+				request.SetBody(*body)
+			}
+
+			return AsResult(request.Execute(*method, url))
 		},
 	}.Run()
 	res.Print()
